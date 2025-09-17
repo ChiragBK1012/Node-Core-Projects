df2ed78bd7e243829843e0d48dffed7c0978745d
Chirag <chiragbk1012@gmail.com>
2025-09-17 21:47:37 +0530

    updated serverGetPost.js

--- basic-server/serverGetPost.js
+++ basic-server/serverGetPost.js
@@ -27,7 +27,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
